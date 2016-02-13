7bb0223516dc227bf5e89dbf266688156e7886d8
Desktop <gabrielrubinstein1@googlemail.com>
2016-02-13 02:47:52 +0000

    testing post from email

--- server.js
+++ server.js
@@ -10,11 +10,15 @@ http.listen(8080, function(){
   console.log('listening on *:8080');
 });
 
-mailin.start({
-    port: 25,
-    disableWebhook:true
-});
-
-mailin.on('message', function(connection, data, content) {
-    console.log("received email!");
+app.post('/', function(req, res, next) {
+    //req.form.complete(function(err, fields, files) {
+    //if (err) { next(err); }
+    //else {
+    //        console.log(fields);
+    //        console.log('---------------');
+    //        console.log(files);
+    //        res.redirect(req.url);
+    //    }
+    //});
+    console.log('received post');
 });
