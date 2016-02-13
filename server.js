4ef10b16eb50bd738d8e875e0afabdacddc014c0
Desktop <gabrielrubinstein1@googlemail.com>
2016-02-13 03:18:16 +0000

    trying to make post handling work

--- server.js
+++ server.js
@@ -6,11 +6,26 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
+
+app.post('/incoming_mail', function(req, res){
+  var form = new formidable.IncomingForm()
+  form.parse(req, function(err, fields, files) {
+    console.log(fields.from)
+    console.log(fields.headers['Subject'])
+    console.log(fields.plain)
+    console.log(fields.html)
+    console.log(fields.reply_plain)
+    res.writeHead(200, {'content-type': 'text/plain'})
+    res.end('Message Received. Thanks!\r\n')
+  })
+})
+
+
 http.listen(8080, function(){
   console.log('listening on *:8080');
 });
 
-app.post('/', function(req, res, next) {
+//app.post('/', function(req, res, next) {
     //req.form.complete(function(err, fields, files) {
     //if (err) { next(err); }
     //else {
@@ -20,5 +35,5 @@ app.post('/', function(req, res, next) {
     //        res.redirect(req.url);
     //    }
     //});
-    console.log('received post');
-});
+  //  console.log('received post');
+//});
