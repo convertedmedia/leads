f87f25926ae32639174a9d6f377d58a8d3043319
Desktop <gabrielrubinstein1@googlemail.com>
2016-02-13 00:37:08 +0000

    back to hello world

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '\index.html');
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
+  res.send('<h1>Hello world</h1>');
 });
 
 http.listen(3000, function(){
