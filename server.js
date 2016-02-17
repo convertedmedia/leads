a0f6faa24ff9accad95af036cdef0485d3c6d437
Desktop <gabrielrubinstein1@googlemail.com>
2016-02-17 03:14:00 +0000

    still trying

--- server.js
+++ server.js
@@ -87,7 +87,8 @@ function getLocation(leadData) {
            	} else if (typeof data.registered_country.names.en !== undefined && data.registered_country.names.en.length) {
                 leadData["Country"] = data.registered_country.names.en;
            	};
-           	if (typeof data.subdivisions !== undefined && typeof data.subdivisions[0] !== undefined && typeof data.subdivisions[0].iso_code !== undefined && data.subdivisions[0].iso_code.length) {
+           	console.log(JSON.stringify(data);
+           	if (typeof data.subdivisions !== undefined && typeof data.subdivisions[0] !== undefined &&  typeof data.subdivisions[0].iso_code !== undefined && data.subdivisions[0].iso_code.length) {
                	leadData.StateProvince = data.subdivisions[0].iso_code;
            	};
            	if (typeof data.traits.autonomous_system_organization !== undefined && data.traits.autonomous_system_organization.length) {
