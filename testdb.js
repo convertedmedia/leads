958afc8f623c2dcd134aef579993c33164c6c9e9
convertedmedia <gabriel@kitmondo.com>
2016-03-24 01:46:26 +0000

    made db

--- testdb.js
+++ testdb.js
@@ -8,16 +8,12 @@ var db = new sqlite3.Database(file);
 
 db.serialize(function() {
 	if(!exists) {
-		db.run("CREATE TABLE test_db
-			(
-				id int NOT NULL PRIMARY KEY,
-				Name varchar(255) NOT NULL
-			)
+		db.run("CREATE TABLE testdb(id int NOT NULL PRIMARY KEY,Name varchar(255) NOT NULL)")
 	};
 	
-	var stmt = db.prepare("INSERT INTO test_db VALUES (?)");
+	var stmt = db.prepare("INSERT INTO testdb VALUES (1, 'something')");
 	
-	stmt.run(1, "Something";
+	stmt.run;
 	
 	stmt.finalize();
 	
