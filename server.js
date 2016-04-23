9563778ce5fc7ea83df4304e9521dcbe633c5aed
Desktop <gabrielrubinstein1@googlemail.com>
2016-04-23 14:21:49 +0100

    removeda  line

--- server.js
+++ server.js
@@ -196,7 +196,6 @@ io.on('connection', function(socket){
 
 function processLead(leadData, type) {
 	var dbData = processLeadData(leadData);
-	dbData.id = uid
 	dbData.email = dbData.email.toLowerCase();
 	connection.query('SELECT * FROM contact WHERE contact_email = ?;', [dbData.email], function(err, results, fields) {
 		if (results.length == 1) {
